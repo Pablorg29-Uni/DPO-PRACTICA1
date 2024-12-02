5598a519bd2f8b09349334597c3c93c9a7c22299
dav <david.grodriguez@students.salle.url.edu>
2024-12-02 11:27:23 +0100

    ITEMS

---
+++ src/Business/Items.java
@@ -0,0 +1,41 @@
+package Business;
+
+public class Items {
+    private int id;
+    private String name;
+    private int power;
+    private int durability;
+    private String classe;
+
+
+
+    public Items(int id, String name, int power, int durability, String classe) {
+        this.id = id;
+        this.name = name;
+        this.power = power;
+        this.durability = durability;
+        this.classe = classe;
+    }
+
+    // Métodos getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getDurability() {
+        return durability;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+}
